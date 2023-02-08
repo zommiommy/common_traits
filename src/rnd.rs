aaef52ed89b1e9855f639a6cfc9317c1a62b92c2
Fontana Tommaso <tommaso.fontana.ext@wuerth-phoenix.com>
2023-02-08 14:52:08 +0100

    added rng doc example

--- src/rnd.rs
+++ src/rnd.rs
@@ -1,4 +1,35 @@
 /// A generic Random number generator
+///
+/// # Example
+/// 
+/// ```rust
+/// use common_traits::{Rng, RngNext};
+/// 
+/// pub struct Xorshift64(u64);
+/// 
+/// impl Rng for Xorshift64 {
+///     fn new(seed: u64) -> Self {
+///         Self(seed.saturating_add(1))
+///     }
+/// }
+/// 
+/// impl RngNext<u64> for Xorshift64 {
+///     fn next_inner(&mut self) -> u64 {
+///         self.0 ^= self.0 << 13;
+///         self.0 ^= self.0 >> 7;
+///         self.0 ^= self.0 << 17;
+///         self.0
+///     }
+/// }
+/// 
+/// impl RngNext<f64> for Xorshift64 {
+///     fn next_inner(&mut self) -> f64 { 
+///         let v: u64 = (self.next::<u64>() >> 11) | (1023 << 52);
+///         let r: f64 = f64::from_le_bytes(v.to_le_bytes());
+///         r - 1f64
+///     }
+/// }
+/// ```
 pub trait Rng {
     /// Instantiate a new Rng making no assumptions on its seed.
     fn new(seed: u64) -> Self;
@@ -13,6 +44,37 @@ pub trait Rng {
 }
 
 /// Implementation of a specific type generation for a Rng
+/// 
+/// # Example
+/// 
+/// ```rust
+/// use common_traits::{Rng, RngNext};
+/// 
+/// pub struct Xorshift64(u64);
+/// 
+/// impl Rng for Xorshift64 {
+///     fn new(seed: u64) -> Self {
+///         Self(seed.saturating_add(1))
+///     }
+/// }
+/// 
+/// impl RngNext<u64> for Xorshift64 {
+///     fn next_inner(&mut self) -> u64 {
+///         self.0 ^= self.0 << 13;
+///         self.0 ^= self.0 >> 7;
+///         self.0 ^= self.0 << 17;
+///         self.0
+///     }
+/// }
+/// 
+/// impl RngNext<f64> for Xorshift64 {
+///     fn next_inner(&mut self) -> f64 { 
+///         let v: u64 = (self.next::<u64>() >> 11) | (1023 << 52);
+///         let r: f64 = f64::from_le_bytes(v.to_le_bytes());
+///         r - 1f64
+///     }
+/// }
+/// ```
 pub trait RngNext<T> {
     fn next_inner(&mut self) -> T;
 }
