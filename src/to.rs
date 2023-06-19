fec7b850d8e41063bf3f076c3e0f75a2b2a7c3af
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-06-19 20:52:16 +0200

    fixed symmetry of to

--- src/to.rs
+++ src/to.rs
@@ -12,6 +12,12 @@ $(
             self as $ty
         }
     }
+    impl To<$ty1> for $ty {
+        #[inline(always)]
+        fn to(self) -> $ty1 {
+            self as $ty1
+        }
+    }
 )*
 
 impl_to!($($ty,)*);
@@ -21,3 +27,60 @@ impl_to!($($ty,)*);
 }
 
 impl_to!(u8, i8, u16, i16, u32, i32, u64, i64, u128, i128, f32, f64,);
+
+#[cfg(feature="half")]
+impl To<half::f16> for f32 {
+    #[inline(always)]
+    fn to(self) -> half::f16 {
+        half::f16::from_f32(self)
+    }
+}
+#[cfg(feature="half")]
+impl To<half::bf16> for f32 {
+    #[inline(always)]
+    fn to(self) -> half::bf16 {
+        half::bf16::from_f32(self)
+    }
+}
+#[cfg(feature="half")]
+impl To<half::f16> for f64 {
+    #[inline(always)]
+    fn to(self) -> half::f16 {
+        half::f16::from_f64(self)
+    }
+}
+#[cfg(feature="half")]
+impl To<half::bf16> for f64 {
+    #[inline(always)]
+    fn to(self) -> half::bf16 {
+        half::bf16::from_f64(self)
+    }
+}
+#[cfg(feature="half")]
+impl To<f32> for half::f16 {
+    #[inline(always)]
+    fn to(self) -> f32 {
+        self.to_f32()
+    }
+}
+#[cfg(feature="half")]
+impl To<f32> for half::bf16 {
+    #[inline(always)]
+    fn to(self) -> f32 {
+        self.to_f32()
+    }
+}
+#[cfg(feature="half")]
+impl To<f64> for half::f16 {
+    #[inline(always)]
+    fn to(self) -> f64 {
+        self.to_f64()
+    }
+}
+#[cfg(feature="half")]
+impl To<f64> for half::bf16 {
+    #[inline(always)]
+    fn to(self) -> f64 {
+        self.to_f64()
+    }
+}
