9539c1087b98e20b709b289c5db0b14ae5116a54
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-07-31 19:31:44 +0200

    changed benches to better reflect the usage of fast_div fast_mod and fast_range

--- benches/fastmod.rs
+++ benches/fastmod.rs
@@ -9,189 +9,197 @@ use common_traits::*;
 use test::{black_box, Bencher};
 
 #[bench]
-fn test_div_u8(b: &mut Bencher) {
+fn test_u8_div(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u8>::MAX) as u8;
     b.iter(|| {
         let a = rng.gen_range(0..<u8>::MAX) as u8;
-        let d = rng.gen_range(2..<u8>::MAX) as u8;
         black_box(a) / black_box(d)
     })
 }
 #[bench]
-fn test_mod_u8(b: &mut Bencher) {
+fn test_u8_mod(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u8>::MAX) as u8;
     b.iter(|| {
         let a = rng.gen_range(0..<u8>::MAX) as u8;
-        let d = rng.gen_range(2..<u8>::MAX) as u8;
         black_box(a) % black_box(d)
     })
 }
 
 #[bench]
-fn test_fast_div_u8(b: &mut Bencher) {
+fn test_u8_fast_div(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u8>::MAX) as u8;
+    let mask = d.compute_mask_fast();
     b.iter(|| {
         let a = rng.gen_range(0..<u8>::MAX) as u8;
-        let d = rng.gen_range(2..<u8>::MAX) as u8;
-        black_box(a).fast_div(black_box(d))
+        black_box(a).fast_div_mask(black_box(mask))
     })
 }
 #[bench]
-fn test_fast_mod_u8(b: &mut Bencher) {
+fn test_u8_fast_mod(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u8>::MAX) as u8;
+    let mask = d.compute_mask_fast();
     b.iter(|| {
         let a = rng.gen_range(0..<u8>::MAX) as u8;
-        let d = rng.gen_range(2..<u8>::MAX) as u8;
-        black_box(a).fast_mod(black_box(d))
+        black_box(a).fast_mod_mask(black_box(d), black_box(mask))
     })
 }
 #[bench]
-fn test_fast_range_u8(b: &mut Bencher) {
+fn test_u8_fast_range(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u8>::MAX) as u8;
     b.iter(|| {
         let a = rng.gen_range(0..<u8>::MAX) as u8;
-        let d = rng.gen_range(2..<u8>::MAX) as u8;
         black_box(a).fast_range(black_box(d))
     })
 }
 
 #[bench]
-fn test_div_u16(b: &mut Bencher) {
+fn test_u16_div(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u16>::MAX) as u16;
     b.iter(|| {
         let a = rng.gen_range(0..<u16>::MAX) as u16;
-        let d = rng.gen_range(2..<u16>::MAX) as u16;
         black_box(a) / black_box(d)
     })
 }
 #[bench]
-fn test_mod_u16(b: &mut Bencher) {
+fn test_u16_mod(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u16>::MAX) as u16;
     b.iter(|| {
         let a = rng.gen_range(0..<u16>::MAX) as u16;
-        let d = rng.gen_range(2..<u16>::MAX) as u16;
         black_box(a) % black_box(d)
     })
 }
 
 #[bench]
-fn test_fast_div_u16(b: &mut Bencher) {
+fn test_u16_fast_div(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u16>::MAX) as u16;
+    let mask = d.compute_mask_fast();
     b.iter(|| {
         let a = rng.gen_range(0..<u16>::MAX) as u16;
-        let d = rng.gen_range(2..<u16>::MAX) as u16;
-        black_box(a).fast_div(black_box(d))
+        black_box(a).fast_div_mask(black_box(mask))
     })
 }
 #[bench]
-fn test_fast_mod_u16(b: &mut Bencher) {
+fn test_u16_fast_mod(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u16>::MAX) as u16;
+    let mask = d.compute_mask_fast();
     b.iter(|| {
         let a = rng.gen_range(0..<u16>::MAX) as u16;
-        let d = rng.gen_range(2..<u16>::MAX) as u16;
-        black_box(a).fast_mod(black_box(d))
+        black_box(a).fast_mod_mask(black_box(d), black_box(mask))
     })
 }
 #[bench]
-fn test_fast_range_u16(b: &mut Bencher) {
+fn test_u16_fast_range(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u16>::MAX) as u16;
     b.iter(|| {
         let a = rng.gen_range(0..<u16>::MAX) as u16;
-        let d = rng.gen_range(2..<u16>::MAX) as u16;
         black_box(a).fast_range(black_box(d))
     })
 }
 
 #[bench]
-fn test_div_u32(b: &mut Bencher) {
+fn test_u32_div(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u32>::MAX) as u32;
     b.iter(|| {
         let a = rng.gen_range(0..<u32>::MAX) as u32;
-        let d = rng.gen_range(2..<u32>::MAX) as u32;
         black_box(a) / black_box(d)
     })
 }
 #[bench]
-fn test_mod_u32(b: &mut Bencher) {
+fn test_u32_mod(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u32>::MAX) as u32;
     b.iter(|| {
         let a = rng.gen_range(0..<u32>::MAX) as u32;
-        let d = rng.gen_range(2..<u32>::MAX) as u32;
         black_box(a) % black_box(d)
     })
 }
 
 #[bench]
-fn test_fast_div_u32(b: &mut Bencher) {
+fn test_u32_fast_div(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u32>::MAX) as u32;
+    let mask = d.compute_mask_fast();
     b.iter(|| {
         let a = rng.gen_range(0..<u32>::MAX) as u32;
-        let d = rng.gen_range(2..<u32>::MAX) as u32;
-        black_box(a).fast_div(black_box(d))
+        black_box(a).fast_div_mask(black_box(mask))
     })
 }
 #[bench]
-fn test_fast_mod_u32(b: &mut Bencher) {
+fn test_u32_fast_mod(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u32>::MAX) as u32;
+    let mask = d.compute_mask_fast();
     b.iter(|| {
         let a = rng.gen_range(0..<u32>::MAX) as u32;
-        let d = rng.gen_range(2..<u32>::MAX) as u32;
-        black_box(a).fast_mod(black_box(d))
+        black_box(a).fast_mod_mask(black_box(d), black_box(mask))
     })
 }
 #[bench]
-fn test_fast_range_u32(b: &mut Bencher) {
+fn test_u32_fast_range(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u32>::MAX) as u32;
     b.iter(|| {
         let a = rng.gen_range(0..<u32>::MAX) as u32;
-        let d = rng.gen_range(2..<u32>::MAX) as u32;
         black_box(a).fast_range(black_box(d))
     })
 }
 
 #[bench]
-fn test_div_u64(b: &mut Bencher) {
+fn test_u64_div(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u64>::MAX) as u64;
     b.iter(|| {
         let a = rng.gen_range(0..<u64>::MAX) as u64;
-        let d = rng.gen_range(2..<u64>::MAX) as u64;
         black_box(a) / black_box(d)
     })
 }
 #[bench]
-fn test_mod_u64(b: &mut Bencher) {
+fn test_u64_mod(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u64>::MAX) as u64;
     b.iter(|| {
         let a = rng.gen_range(0..<u64>::MAX) as u64;
-        let d = rng.gen_range(2..<u64>::MAX) as u64;
         black_box(a) % black_box(d)
     })
 }
 
 #[bench]
-fn test_fast_div_u64(b: &mut Bencher) {
+fn test_u64_fast_div(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u64>::MAX) as u64;
+    let mask = d.compute_mask_fast();
     b.iter(|| {
         let a = rng.gen_range(0..<u64>::MAX) as u64;
-        let d = rng.gen_range(2..<u64>::MAX) as u64;
-        black_box(a).fast_div(black_box(d))
+        black_box(a).fast_div_mask(black_box(mask))
     })
 }
 #[bench]
-fn test_fast_mod_u64(b: &mut Bencher) {
+fn test_u64_fast_mod(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u64>::MAX) as u64;
+    let mask = d.compute_mask_fast();
     b.iter(|| {
         let a = rng.gen_range(0..<u64>::MAX) as u64;
-        let d = rng.gen_range(2..<u64>::MAX) as u64;
-        black_box(a).fast_mod(black_box(d))
+        black_box(a).fast_mod_mask(black_box(d), black_box(mask))
     })
 }
 #[bench]
-fn test_fast_range_u64(b: &mut Bencher) {
+fn test_u64_fast_range(b: &mut Bencher) {
     let mut rng = SmallRng::seed_from_u64(0);
+    let d = rng.gen_range(2..<u64>::MAX) as u64;
     b.iter(|| {
         let a = rng.gen_range(0..<u64>::MAX) as u64;
-        let d = rng.gen_range(2..<u64>::MAX) as u64;
         black_box(a).fast_range(black_box(d))
     })
 }
